f7cf9c67f3a12579c201c5306e73edbf1e15d749
Dias <dias.atudinov@gmail.com>
2025-02-09 22:25:20 +0500

    Settings View add

--- Sweet Nemesis/Sweet Nemesis/Views/Settings/SettingsView.swift
+++ Sweet Nemesis/Sweet Nemesis/Views/Settings/SettingsView.swift
@@ -1,10 +1,12 @@
+
+
 import SwiftUI
 import StoreKit
 
 struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var settings: SettingsViewModel
-
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -12,128 +14,90 @@ struct SettingsView: View {
                 VStack(spacing: 0)  {
                     Spacer()
                     ZStack {
-                        HStack(spacing: 0) {
+                        
+                        Image(.settingsBg)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        VStack(spacing: DeviceInfo.shared.deviceType == .pad ? 24:6) {
                             
-                            VStack {
-                                Spacer()
-                                Image(.lady1Position)
-                                    .resizable()
-                                    .scaledToFit()
-                            }.ignoresSafeArea(edges: [.horizontal, .bottom])
+                            Text("Settings")
+                                .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 80:40))
+                                .textCase(.uppercase)
+                                .foregroundStyle(.white)
                             
-                            VStack(spacing: DeviceInfo.shared.deviceType == .pad ? 30:15) {
+                            VStack(spacing: DeviceInfo.shared.deviceType == .pad ? 16:4)  {
                                 
-                                VStack(spacing: DeviceInfo.shared.deviceType == .pad ? 30:15)  {
-                                    
-                                    Text("Music")
-                                        .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 64:32))
-                                        .textCase(.uppercase)
-                                        .foregroundStyle(.yellow)
-                                    HStack(spacing:DeviceInfo.shared.deviceType == .pad ? 40: 20) {
-                                        Text("Off")
-                                            .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 48:24))
-                                            .textCase(.uppercase)
-                                            .foregroundStyle(.yellow)
-                                        Button {
-                                            withAnimation {
-                                                settings.musicEnabled.toggle()
-                                            }
-                                        } label: {
-                                            if settings.musicEnabled {
-                                                Image(.onLLG)
-                                                    .resizable()
-                                                    .scaledToFit()
-                                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 68:34)
-                                            } else {
-                                                Image(.offLLG)
-                                                    .resizable()
-                                                    .scaledToFit()
-                                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 68:34)
-                                            }
+                                Text("Music")
+                                    .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 64:32))
+                                    .textCase(.uppercase)
+                                    .foregroundStyle(.white)
+                                
+                                    Button {
+                                        withAnimation {
+                                            settings.musicEnabled.toggle()
                                         }
-                                        
-                                        Text("On")
-                                            .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 48:24))
-                                            .textCase(.uppercase)
-                                            .foregroundStyle(.yellow)
+                                    } label: {
+                                        if settings.musicEnabled {
+                                            Image(.onSN)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 104:52)
+                                        } else {
+                                            Image(.offSN)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 104:52)
+                                        }
+                                    }
+                                
+                            }
+                            
+                            VStack(spacing: DeviceInfo.shared.deviceType == .pad ? 16:4)  {
+                                
+                                Text("Vibration")
+                                    .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 64 :32))
+                                    .textCase(.uppercase)
+                                    .foregroundStyle(.white)
+                                
+                                
+                                    Button {
+                                        withAnimation {
+                                            settings.soundEnabled.toggle()
+                                        }
+                                    } label: {
+                                        if settings.soundEnabled {
+                                            Image(.onSN)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 104:52)
+                                        } else {
+                                            Image(.offSN)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 104:52)
+                                        }
                                     }
                                     
-                                }
-                                
-                                VStack(spacing: DeviceInfo.shared.deviceType == .pad ? 30:15)  {
-                                    
-                                    Text("Vibration")
-                                        .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 64 :32))
+                            }
+                            Button {
+                                rateUs()
+                            } label: {
+                                ZStack {
+                                    Image(.rateUsBg)
+                                        .resizable()
+                                        .scaledToFit()
+                                    Text("Rate us")
+                                        .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 48:24))
+                                        .foregroundStyle(.white)
                                         .textCase(.uppercase)
-                                        .foregroundStyle(.yellow)
+                                }.frame(height: DeviceInfo.shared.deviceType == .pad ? 88:44)
                                     
-                                    HStack(spacing: DeviceInfo.shared.deviceType == .pad ? 40:20) {
-                                        Text("Off")
-                                            .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 48:24))
-                                            .textCase(.uppercase)
-                                            .foregroundStyle(.yellow)
-                                        Button {
-                                            withAnimation {
-                                                settings.soundEnabled.toggle()
-                                            }
-                                        } label: {
-                                            if settings.soundEnabled {
-                                                Image(.onLLG)
-                                                    .resizable()
-                                                    .scaledToFit()
-                                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 68:34)
-                                            } else {
-                                                Image(.offLLG)
-                                                    .resizable()
-                                                    .scaledToFit()
-                                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 68:34)
-                                            }
-                                        }
-                                        
-                                        Text("on")
-                                            .font(.custom(Fonts.bold.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 48:24))
-                                            .textCase(.uppercase)
-                                            .foregroundStyle(.yellow)
-                                    }
-                                }
-                                Button {
-                                    rateUs()
-                                } label: {
-                                    
-                                    Text("RATE US")
-                                        .font(.custom(Fonts.regular.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 40:20))
-                                        .foregroundStyle(.yellow)
-                                        .textCase(.uppercase)
-                                        .padding(.vertical, DeviceInfo.shared.deviceType == .pad ? 16:8)
-                                        .padding(.horizontal, DeviceInfo.shared.deviceType == .pad ? 80:40)
-                                        .background(
-                                            Color.loaderBg
-                                        )
-                                        .cornerRadius(20)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 20)
-                                                .stroke(Color.yellow, lineWidth: 1)
-                                        )
-                                }
-                                
-                            }.padding(DeviceInfo.shared.deviceType == .pad ? 40:20)
-                                .padding(.horizontal, DeviceInfo.shared.deviceType == .pad ? 40:20)
-                                .frame(width: DeviceInfo.shared.deviceType == .pad ?600:300)
-                                .background(
-                                    Color.mainRed
-                                )
-                                .cornerRadius(10)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.yellow, lineWidth: 1)
-                                )
-                            VStack {
-                                Spacer()
-                                Image(.lady1Position)
-                                    .resizable()
-                                    .scaledToFit()
-                            }.ignoresSafeArea(edges: [.horizontal, .bottom]).opacity(0)
-                        }.frame(height: geometry.size.height * 0.87)
+                            }
+                            
+                        }
+                        
+                        
                         
                     }
                     
@@ -147,7 +111,7 @@ struct SettingsView: View {
                                 presentationMode.wrappedValue.dismiss()
                             } label: {
                                 ZStack {
-                                    Image(.backLLG)
+                                    Image(.backSN)
                                         .resizable()
                                         .scaledToFit()
                                     
@@ -161,7 +125,7 @@ struct SettingsView: View {
                 }
                 
             }.background(
-                Image(.bg)
+                Image(.bgSN)
                     .resizable()
                     .ignoresSafeArea()
                     .scaledToFill()
--- Sweet Nemesis/Sweet Nemesis/Views/Settings/SettingsViewModel.swift
+++ Sweet Nemesis/Sweet Nemesis/Views/Settings/SettingsViewModel.swift
@@ -1,3 +1,4 @@
+
 import SwiftUI
 
 class SettingsViewModel: ObservableObject {
