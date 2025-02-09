68ec4842ee905dcc4285b05a14ec66085925525e
Dias <dias.atudinov@gmail.com>
2025-02-09 21:52:34 +0500

    first commit

---
+++ Sweet Nemesis/Sweet Nemesis/Sweet_NemesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet_NemesisApp.swift
+//  Sweet Nemesis
+//
+//  Created by Dias Atudinov on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Sweet_NemesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
