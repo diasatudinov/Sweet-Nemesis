bb261ce76bf1ed7522c378a844ab4f213c1e0852
Dias <dias.atudinov@gmail.com>
2025-02-10 14:56:08 +0500

    info view add

--- Sweet Nemesis/Sweet Nemesis/Views/Info/InfoView.swift
+++ Sweet Nemesis/Sweet Nemesis/Views/Info/InfoView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct InfoView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Image(.infoBg)
+                    .resizable()
+                    .scaledToFit()
+            }.padding()
+            VStack {
+                ZStack {
+                    
+                    HStack {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            ZStack {
+                                Image(.backSN)
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                            }.frame(height: DeviceInfo.shared.deviceType == .pad ? 100:50)
+                            
+                        }
+                        Spacer()
+                        
+                    }.padding()
+                }
+                Spacer()
+            }
+        }.background(
+            Image(.bgSN)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+                .blur(radius: 4)
+            
+        )
     }
 }
 
